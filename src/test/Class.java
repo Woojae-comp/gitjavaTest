b5a53b8272b3568f0c0b11c71d0e999f6681df3e
Woojae-comp <chawj91@gmail.com>
2024-05-03 09:40:08 +0900

    X, Y add

--- src/test/Class.java
+++ src/test/Class.java
@@ -4,6 +4,17 @@ public class Class {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int x = 100;
+		int y = 120;
+		System.out.println(x + y);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
